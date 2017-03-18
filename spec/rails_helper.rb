9cf5ca143484c2ea7cbf4f3a8ec29d99b176b1c0
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2017-03-18 18:35:17 +0800

    setup rspec, capybara, database_cleaner, shoulda_matchers, factory_girl, ffaker

---
+++ spec/rails_helper.rb
@@ -0,0 +1,29 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'capybara/rspec'
+require 'shoulda/matchers'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+# Checks for pending migration and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.maintain_test_schema!
+
+# Should Matches config
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+end
