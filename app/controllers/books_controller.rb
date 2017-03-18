d79fd3f5b88e092385aea78cb586342b4ef97370
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2017-03-18 23:08:34 +0800

    done with create books spec

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,26 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      flash[:success] = 'Book has successfully been created.'
+      redirect_to books_path
+    else
+      flash[:danger] = 'There are some errors.'
+      render :new
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :description, :price)
   end
 end
--- spec/features/books/create_spec.rb
+++ spec/features/books/create_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'User submits a book' do
-  let(:book) { build :book }
+  let!(:book) { FactoryGirl.build(:book) }
 
   scenario 'they see the page for the submitted book' do
     visit root_path
@@ -10,9 +10,9 @@ RSpec.feature 'User submits a book' do
     fill_in 'book_title', with: book.title
     fill_in 'book_description', with: book.description
     fill_in 'book_price', with: 30_000
-    click 'Submit'
+    click_button 'Create a Book'
 
-    expect(page).to have_content 'Book successfully created.'
+    expect(page).to have_content 'Book has successfully been created.'
     expect(page).to have_content book.title
   end
 end
