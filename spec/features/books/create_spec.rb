8f7670054777c3060dc067759a563492f8a65813
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2017-03-19 10:31:05 +0800

    done with create spec

--- spec/features/books/create_spec.rb
+++ spec/features/books/create_spec.rb
@@ -15,4 +15,17 @@ RSpec.feature 'User submits a book' do
     expect(page).to have_content 'Book has successfully been created.'
     expect(page).to have_content book.title
   end
+
+  context 'the form is invalid' do
+    scenario 'they see a useful error message' do
+      visit root_path
+      click_on 'Add new book'
+
+      fill_in 'book_description', with: book.description
+      fill_in 'book_price', with: 30_000
+      click_button 'Create a Book'
+
+      expect(page).to have_content "Title can't be blank"
+    end
+  end
 end
